847de4e494e46287c2d80b1dfb7e4ae9c40b5d4a
Stian Didriksen <stipsan@gmail.com>
2016-08-23 21:24:52 +0200

    Adding label support

--- src/__tests__/renderInput-test.js
+++ src/__tests__/renderInput-test.js
@@ -1,11 +1,38 @@
 import renderer from 'react-test-renderer'
 
-import renderInput from '../renderInput'
+import RenderInput from '../renderInput'
+
+// Capitalize to render it outside of redux-form
 
 describe('renderInput', () => {
+  // @TODO investigate if we can just grab the default props directly from redux-form/Field
+  const defaultProps = {
+    input: {
+      name: 'email',
+      onBlur: () => {},
+      onChange: () => {},
+      onDragStart: () => {},
+      onDrop: () => {},
+      onFocus: () => {},
+      value: '',
+    },
+    meta: {
+      asyncValidating: false,
+      dirty: false,
+      invalid: true,
+      pristine: true,
+      submitting: false,
+      touched: false,
+      valid: false,
+    },
+  }
   it('renders correctly', () => {
     expect(renderer.create(
-      <renderInput />
+      <RenderInput {...defaultProps} />
+    ).toJSON()).toMatchSnapshot()
+
+    expect(renderer.create(
+      <RenderInput {...defaultProps} label="E-mail" />
     ).toJSON()).toMatchSnapshot()
   })
 })
--- src/renderInput.js
+++ src/renderInput.js
@@ -1,35 +1,59 @@
 import cx from 'classnames'
 import { PropTypes } from 'react'
-import { Input as UkInput } from 'uikit-react'
+import { Input } from 'uikit-react'
 // import FieldPropTypes from 'redux-form-uikit/PropTypes/Field'
 
-const Input = ({
+const renderInput = ({
   input,
   meta: { asyncValidating, touched, error, submitting },
   icon,
   label,
   ...custom,
 }) => {
+  const props = {
+    autoComplete: input.name,
+    placeholder: label,
+    id: label ? input.name : undefined,
+    ...input,
+  }
+  const component = (
+    <Input
+      autoComplete={input.name}
+      placeholder={label}
+      {...input}
+      {...custom}
+      danger={touched && error}
+    />
+  )
+  const help = touched && error && (
+    <p className="uk-form-help-block uk-text-left">
+      {error}
+    </p>
+  )
+  if (label) {
+    const id = input.id || input.name
+    return (
+      <div className="uk-form-row">
+        <label className="uk-form-label" htmlFor={id}>{label}</label>
+        <div className="uk-form-controls">
+          {component}
+          {help}
+        </div>
+      </div>
+    )
+  }
   return (
-    <div className={cx('uk-form-row')}>
-      <UkInput
-        autoComplete={input.name}
-        placeholder={label}
-        {...input}
-        {...custom}
-        danger={touched && error}
-      />
-      {touched && error && <p className="uk-form-help-block uk-text-left">
-        {error}
-      </p>}
+    <div className="uk-form-row">
+      {component}
+      {help}
     </div>
   )
 }
 
 
-Input.propTypes = {
+renderInput.propTypes = {
   // ...FieldPropTypes,
   icon: PropTypes.string,
 }
 
-export default Input
+export default renderInput
