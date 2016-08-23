81d316dcc9da6579c4e5145e5b5e061f965cbd35
Stian Didriksen <stipsan@gmail.com>
2016-08-23 19:48:17 +0200

    experiment

--- src/stories/Input.js
+++ src/stories/Input.js
@@ -1,4 +1,5 @@
 import { storiesOf } from '@kadira/storybook'
+import { PropTypes } from 'react'
 import { Field } from 'redux-form'
 import { Button } from 'uikit-react'
 
@@ -7,7 +8,12 @@ import Input from '../Input'
 // This is to work around: https://github.com/kadirahq/react-storybook-addon-info/issues/26#issuecomment-229029177
 Input.displayName = 'Input'
 
+// Make sure only user-land propTypes are shown (and not props relevant to redux-form internals)
 const InputProps = () => <input />
+InputProps.propTypes = {
+  icon: PropTypes.string,
+}
+InputProps.displayName = 'Inputs'
 
 storiesOf('Input', module)
   /* eslint-disable max-len */
@@ -34,7 +40,7 @@ storiesOf('Input', module)
         <Button type="submit">Submit</Button>
       </div>
     </div>
-  ), { header: false, inline: true, propTables: [Input] })
+  ), { header: false, inline: true, propTables: [InputProps] })
   .addWithInfo('Advanced', `
   If you have react-collapse in your dependencies you can pass 'shouldTransitionError' to activate animated reveal and collapse of validation errors.
   `, () => (
@@ -42,5 +48,5 @@ storiesOf('Input', module)
 
       <Field name="email" component={Input} />
     </div>
-  ), { header: false, inline: true })
+  ), { header: false, inline: true, propTables: null })
   /* eslint-enable */
